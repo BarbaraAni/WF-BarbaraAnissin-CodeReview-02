150de287f79f5474a6fd9e3ba4c96e9ec4f11697
Barbara Anissin <anissinb@gmail.com>
2019-10-04 12:50:32 +0200

    corrected js calculation greece

--- js/script.js
+++ js/script.js
@@ -3,14 +3,15 @@ function calculateInsurance() {
     var age = document.getElementById("age").value
     var hp = document.getElementById("hp").value
     var name = document.getElementById("name").value
-    if (land=="austria") {
-    	var ins = hp * 100 / age;
+    if (land == "austria") {
+        var ins = hp * 100 / age;
+    } else if (land == "hungary") {
+        var ins = hp * 120 / age + 100;
+    } else if (land == "greece") {
+        var ins = hp * 150 / (Number(age) + 3) + 50;
+    } else {
+        var ins = "Please try again"
     }
-    else if (land =="hungary") {
-    	var ins = hp * 120 / age + 100;
-    }
-    else {
-    	var ins = hp * 150 / (age+3) + 50;
-    }
-    document.getElementById("placeholder").innerHTML = name + ", your insurance costs " + ins + " €"
+    document.getElementById("placeholder").innerHTML = name + ", your insurance costs " + Math.round(ins * 100) / 100 + " €"
+
 }
